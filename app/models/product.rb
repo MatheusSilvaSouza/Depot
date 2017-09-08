32c57ae5ed2fa2ec8dbccc7cc08352c5f38f3f3c
MatheusSilvaSouza <31622665+MatheusSilvaSouza@users.noreply.github.com>
2017-09-08 15:07:11 -0300

    B2 Unit Test for Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, :price, presence: true
-  validates :price, numericality: {greater_or_equals_to: 0}
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  
+
+  def new_product(title="title", description="description", image_url="", price=1)
+    return Product.new(title:title, description: description,
+                       image_url: image_url, price:price)
+  end
+
+  test 'default product' do
+    product = new_product
+    assert product.valid?,  product.to_json + ' should be valid'
+  end
+
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].blank?
+  end
+
+  test "product price must be positive" do
+    product = new_product
+
+    product.price = -1
+    assert product.invalid?, product.to_json + ' should be invalid'
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?, product.to_json + ' should be invalid'
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0.001
+    assert product.invalid?, product.to_json + ' should be invalid'
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?, product.to_json + ' should be valid'
+  end
+
+  test 'product uniqueness of title' do
+    product = new_product(title=products(:one).title)
+
+    assert product.invalid?, products(:one).to_json + product.to_json
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
 end
