e7e569191ffa9460f22b26ef206683ee1fad1fbe
MatheusSilvaSouza <31622665+MatheusSilvaSouza@users.noreply.github.com>
2017-09-12 12:24:00 -0300

    C5 and Styling

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,9 @@
 class StoreController < ApplicationController
+  before_action :layout_params
+  def layout_params
+    @page_title = 'Catalog'
+  end
+
   def index
     @all_products = Product.order(:title)
   end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get store_index_url
+    get catalog_url
     assert_response :success
     assert_select 'nav.side_nav li', minimum: 2
     assert_select 'h3', 'Title_0'
