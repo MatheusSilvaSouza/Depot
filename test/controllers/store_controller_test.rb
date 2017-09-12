d90b795bdf6827640c5a346740f4edd694ffa3cd
MatheusSilvaSouza <31622665+MatheusSilvaSouza@users.noreply.github.com>
2017-09-12 11:21:33 -0300

    C1+C2+C3+C4 Ceating, Styling and Testing Catalog. +Default layout

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav li', minimum: 2
+    assert_select 'h3', 'Title_0'
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
